21d72353c68d6b35e81739e8ee41c82402ab2ca3
Yesbolova <134151894+Yesbolova@users.noreply.github.com>
2026-02-14 17:41:12 +0500

    Delete video.py

--- video.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-def open_camera(index: int = 0):
-    cap = cv2.VideoCapture(index)
-    if not cap.isOpened():
-        raise RuntimeError(f"Cannot open camera index={index}")
-    return cap
